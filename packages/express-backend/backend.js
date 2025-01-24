876720b2c3564b0d7794899a8351c7afd3240bd4
Kyle Lin <linkyle0924@gmail.com>
2025-01-23 22:00:30 -0800

    Add users by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -45,6 +45,8 @@ const findUserByName = (name) => {
     (user) => user["name"] === name
   );
 };
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
@@ -54,6 +56,16 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
